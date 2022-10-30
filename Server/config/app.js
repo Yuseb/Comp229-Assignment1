e0ad1af7731bfa4ddc4e3e3d590c9817f609d885
Yusuf Esad Bilgic <yusufesadb@outlook.com>
2022-10-30 00:31:03 -0400

    password

--- Server/config/app.js
+++ Server/config/app.js
@@ -4,7 +4,8 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-// authrntication/database setup
+
+// authentication
 let passport = require('passport');
 let passportLocal = require('passport-local');
 let session = require('express-session');
@@ -13,7 +14,7 @@ let flash = require('connect-flash');
 let mongoose = require('mongoose');
 let db = require('./db');
 
-// point mongoose to the DB URI 
+// point mongoose to db URI(Connection Stream)
 mongoose.connect(db.URI, {useNewUrlParser: true, useUnifiedTopology: true});
 let mongoDB = mongoose.connection;
 mongoDB.on('error', console.error.bind(console, 'Connection Error: '));
@@ -26,7 +27,6 @@ let usersRouter = require('../routes/users');
 let contactsRouter = require('../routes/contactList');
 
 let app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
@@ -35,26 +35,54 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../node_modules')));
+app.use(express.static(path.join(__dirname, '../../public')));
+app.use(express.static(path.join(__dirname, '../../node_modules')));
 
+// express session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave: false
+}))
+// passport middleware (initialize passport)
+app.use(passport.initialize());
+app.use(passport.session());
+//initialize flash - to maintain the error message
+app.use(flash());
+//passport user configuration
+//create a user model instance
+let userModel = require('../models/users');
+let User = userModel.user;
+
+// implement a user authentication strategy
+passport.use(User.createStrategy());
+
+//serialize and deserialize the user info
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+//routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contactList', contactsRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error',{title:'Error'});
+  res.render('error', { 
+    title: 'Error',
+    loggedIn: req.isAuthenticated()
+   });
 });
 
 module.exports = app;
